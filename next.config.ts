06d1a11ec37c890ec89dce0332cd1295e494b2a6
AshishAgarwal12 <ashishagarwal@aviraworld.co.in>
2025-01-15 03:10:28 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',  
-  trailingSlash: true, 
+  output: 'export',  // This tells Next.js to export as a static site
+  trailingSlash: true, // Ensures URLs have a trailing slash for static routing
 };
 
 export default nextConfig;
